fb9efbe2e425f232df115147282b479641e9c3a9
Vishal Row Mysore <visrow@gmail.com>
2023-12-10 08:19:03 -0500

    cookGPT data and instructions

---
+++ cookGPT/src/main/java/com/cookgpt/CookGPT.java
@@ -0,0 +1,13 @@
+package com.cookgpt;
+
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CookGPT {
+    public static void main(String[] args) {
+        SpringApplication.run(CookGPT.class, args);
+    }
+}
